0fd0f0d2f49f648c2d6a389144be6fda926d8ec0
Mehmet Doğru <mdogru685@gmail.com>
2023-08-18 16:44:54 +0300

    feat: added global exception

---
+++ libs/filters/all-exception.filter.ts
@@ -0,0 +1,12 @@
+import { Catch, ArgumentsHost } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+
+@Catch()
+export class AllExceptionsFilter extends BaseExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    return response.status(500).json({ exception });
+  }
+}
